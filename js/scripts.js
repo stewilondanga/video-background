3a086664aa0083ab813c5b9521940dfd1d9c8cc1
Stewart ilondanga <stilondanga@gmail.com>
2018-11-08 16:26:07 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -7,16 +7,16 @@ var navigate = (function() {
   });
 })();
 
-/*var vid = document.getElementById("bgvid");
+var vid = document.getElementById("bgvid");
 var pauseButton = document.querySelector("#polina button");
 
 if (window.matchMedia('(prefers-reduced-motion)').matches) {
-    vid.removeAttribute("autoplay");
-    vid.pause();
-    pauseButton.innerHTML = "Paused";
+  vid.removeAttribute("autoplay");
+  vid.pause();
+  pauseButton.innerHTML = "Paused";
 }
 
-function vidFade() {
+/*function vidFade() {
   vid.classList.add("stopfade");
 }
 
